59c642f89c217a99c2121b1bfb6e790f54f94e27
Joltheim <james.wismer24@gmail.com>
2023-07-08 15:57:29 -0400

    Completed 03-06 & 03-07, toggle & user challenge.

--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -20,6 +20,19 @@ const ShortCircuitExamples = () => {
         </div>
       )}
       {user && <SomeComponent name={user.name} />}
+      <h2>Ternary Operator</h2>
+      {/* single line example */}
+      <button className='btn'>{isEditing ? 'edit' : 'add'}</button>
+      {/* multi line example */}
+      {user ? (
+        <div>
+          <h2>hello there, {user.name}</h2>
+        </div>
+      ) : (
+        <div>
+          <h2>please login</h2>
+        </div>
+      )}
     </>
   )
 };
@@ -35,7 +48,12 @@ const SomeComponent = ({ name }) => {
 
 export default ShortCircuitExamples;
 
+//Lessons:
 //OR operator is primarily used to show default values when
 //fetching from an API (i.e. if API doesn't provide info show default)
 
-//AND operator
+//Ternary operator
+//syntax: condition ? expression1 : expression 2;
+//
+//if condition is truthy, return expression1, else return expression2
+//see ln. 24 for example
--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [toggle, setToggle] = useState(false)
+
+  const toggleSwitch = () => {
+    if (toggle == true) {
+      setToggle(false)
+    } else {
+      setToggle(true)
+    }
+  }
+
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <button className="btn" onClick={toggleSwitch}>button</button>
+      {toggle && <h3>Toggle ON!</h3>}
+    </>
+  )
 };
 
 export default ToggleChallenge;
